907707bed425045dd005115f3a9b7a5a01fd8546
Ifeanyik <jax64720@gmail.com>
2022-06-02 15:57:21 +0000

    Calc

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    import calculator_1 as calc
+    call = sys.argv
+    print(call[1], call[3], "Troy")
+    if len(call) != 4:
+        print("Usage: ./100-my_calculator.py {} {} {}".format(call[1], call[2], call[3]))
+        sys.exit(1)
+    a = int(call[1])
+    b = int(call[3])
+    if call[2] == "+":
+        print("{} {} {} = {}".format(a, call[2], b, calc.add(a, b)))
+    elif call[2] == "-":
+        print("{} {} {} = {}".format(a, call[2], b, calc.sub(a, b)))
+    elif call[2] == "*":
+        print("{} {} {} = {}".format(a, call[2], b, calc.mul(a, b)))
+    elif call[2] == "/":
+        print("{} {} {} = {}".format(a, call[2], b, calc.div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        sys.exit(1)
