ed6497b9621184ae43dffa5bfaa2289a84ab7f20
Ifeanyik <jax64720@gmail.com>
2022-06-05 14:36:25 +0000

    Max integer

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,18 +3,8 @@ def max_integer(my_list=[]):
     iter_num = len(my_list)
     if iter_num == 0:
         return None
-    break_string = ""
     current_element = my_list[0]
-    while break_string != "break":
-        step_count = 0
-        for i in range(iter_num):
-            if my_list[i] < 0:
-                my_list[i] *= -1
-            sub = current_element - my_list[i]
-            step_count += 1
-            if sub < 0:
-                current_element = my_list[i]
-                break
-        if step_count == iter_num:
-            break_string = "break"
+    for i in range(iter_num):
+        if current_element < my_list[i]:
+            current_element = my_list[i]
     return current_element
