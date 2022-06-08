f98e4f162fb1ada505fddc682c65afd153ab51c3
Ifeanyik <jax64720@gmail.com>
2022-06-08 21:40:30 +0000

    Squaring values of multidimensional matrix

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda i: [i[0] ** 2, i[1] ** 2, i[2] ** 2], matrix))
+    return list(map(lambda i: list(map(lambda j: j ** 2, i)), matrix))
