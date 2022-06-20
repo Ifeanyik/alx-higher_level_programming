d33d8048616bec3ef1aa3815cbc320981c553e5c
Ifeanyik <jax64720@gmail.com>
2022-06-20 10:52:33 +0000

    Print list integers

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    count = 0
+    for element in range(x):
+        try:
+            print("{:d}".format(my_list[element]), end="")
+            count += 1
+        except TypeError:
+            pass
+        except ValueError:
+            pass
+        except IndexError:
+            print()
+            return count
+    print()
+    return count
+
