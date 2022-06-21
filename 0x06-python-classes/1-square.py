2d19df6051a2f8bbcbbdb196313faf66b7dd02b9
Ifeanyik <jax64720@gmail.com>
2022-06-21 20:49:17 +0000

    Create private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Create Square class with size attribute'''
+
+
+class Square:
+    '''Create private size attribute'''
+    def __init__(self, size):
+        self.__size = size
