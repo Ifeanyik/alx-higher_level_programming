7d5a090314de7c0d84008c3dd86bf54e668e2031
Ifeanyik <jax64720@gmail.com>
2022-06-27 13:52:48 +0000

    Add attributes height and width

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python3
+'''Create Rectangle class'''
+
+
+class Rectangle:
+    '''Defining class attributes'''
+    def __init__(self, width=0, height=0):
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        '''Retrieve width attribute'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''Sets width value and ensures width is an integer'''
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        '''Retrieve height attribute'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''Sets height and ensures it is an integer not less than 0'''
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
