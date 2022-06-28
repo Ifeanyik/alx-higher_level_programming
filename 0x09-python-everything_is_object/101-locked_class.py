bef78b9ffbccc564c7f775b65eed97fc147d8053
Ifeanyik <jax64720@gmail.com>
2022-06-28 20:28:52 +0000

    Error fix

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+'''Implement LockedClass Object'''
+
 
 class LockedClass:
     def __setattr__(self, __name, __value):
+        '''Stops any attribute other than first_name from being set'''
         if __name != "first_name":
-            raise AttributeError("'LockedClass' object has no attribute '{}'".format(__name))
+            run = "'LockedClass' object has no attribute '{}'".format(__name)
+            raise AttributeError(run)
         self.__dict__[__name] = __value
