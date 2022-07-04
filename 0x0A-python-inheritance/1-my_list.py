c18491d0113e77a7d49dc179d5925f06608f109a
Ifeanyik <jax64720@gmail.com>
2022-07-04 14:55:35 +0000

    1-my_list

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''Create class MyList'''
+
+
+class MyList(list):
+    def print_sorted(self):
+        '''Sorts MyList object and prints it'''
+        self.sort
+        print(self)
