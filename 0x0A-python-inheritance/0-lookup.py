e5fd8263bbc6bf886a784c484d68091258ada85b
Ifeanyik <jax64720@gmail.com>
2022-07-04 13:07:32 +0000

    Error catching

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,8 +5,9 @@
 def lookup(obj):
     '''Returns list of object attributes'''
     ma_list = list()
-    if obj is None:
+    try:
+        for i in obj.__dict__:
+            ma_list.append(i)
+    except Exception:
         return ma_list
-    for i in obj.__dict__:
-        ma_list.append(i)
     return ma_list
