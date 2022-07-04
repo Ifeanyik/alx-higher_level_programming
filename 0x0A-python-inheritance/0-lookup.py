35f33ebc35d5868a782de8aab25c2a0815977476
Ifeanyik <jax64720@gmail.com>
2022-07-04 12:54:18 +0000

    Edge cases

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -5,6 +5,8 @@
 def lookup(obj):
     '''Returns list of object attributes'''
     ma_list = list()
+    if obj is None:
+        return ma_list
     for i in obj.__dict__:
         ma_list.append(i)
     return ma_list
