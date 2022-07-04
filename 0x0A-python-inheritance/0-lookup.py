0de9d514207dca92c6a3a75a922b10d2075a6090
Ifeanyik <jax64720@gmail.com>
2022-07-04 13:12:47 +0000

    H

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -4,10 +4,4 @@
 
 def lookup(obj):
     '''Returns list of object attributes'''
-    ma_list = list()
-    try:
-        for i in obj.__dict__:
-            ma_list.append(i)
-    except Exception:
-        return ma_list
-    return ma_list
+    return dir(obj)
