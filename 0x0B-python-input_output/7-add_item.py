e9b76e0c4b3e83818d73568929ca2d4171d31a82
Ifeanyik <jax64720@gmail.com>
2022-07-05 21:23:15 +0000

    Add JSONitem

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+'''Module that adds all arguments to a python list and
+saves to a json file'''
+
+
+import sys
+import os
+
+
+save_file = __import__('7-save_to_json_file').save_to_json_file
+load_file = __import__('8-load_from_json_file').load_from_json_file
+
+argv_list = []
+if os.path.exists("add_item.json"):
+    argv_list = load_file("add_item.json")
+for i in sys.argv:
+    argv_list.append(i)
+save_file(argv," add_item.json")
