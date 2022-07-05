27c0d8fa12b8b73a13fa33bf3f13e98d15b20c8b
Ifeanyik <jax64720@gmail.com>
2022-07-05 21:30:29 +0000

    Error fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -5,15 +5,15 @@ saves to a json file
 
 
 import sys
-import os
+import os.path
 
 
-save_file = __import__('7-save_to_json_file').save_to_json_file
-load_file = __import__('8-load_from_json_file').load_from_json_file
+save_file = __import__('5-save_to_json_file').save_to_json_file
+load_file = __import__('6-load_from_json_file').load_from_json_file
 
 argv_list = []
 if os.path.exists("add_item.json"):
     argv_list = load_file("add_item.json")
 for i in sys.argv:
     argv_list.append(i)
-save_file(argv," add_item.json")
+save_file(argv_list," add_item.json")
