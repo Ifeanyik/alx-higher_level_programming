26b8681317c3e8d965da243bae49a969e0cf0e6d
Ifeanyik <jax64720@gmail.com>
2022-07-05 21:26:12 +0000

    Error fix

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
-'''Module that adds all arguments to a python list and
-saves to a json file'''
+"""Module that adds all arguments to a python list and
+saves to a json file
+"""
 
 
 import sys
