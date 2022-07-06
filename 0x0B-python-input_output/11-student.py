675dd7d0641910c5f7320d0374de4af6d51f848b
Ifeanyik <jax64720@gmail.com>
2022-07-06 10:04:46 +0000

    Replacing instance attributes

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+'''Defines class Student'''
+
+
+class Student:
+    '''Defining Student object attributes'''
+    def __init__(self, first_name, last_name, age):
+        '''Initialising object attributes'''
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        '''Retrieve dict representation of obj attributes'''
+        res = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if type(i) is str:
+                    if hasattr(self, i):
+                        res[i] = self.__dict__[i]
+            return res
+        return self.__dict__.copy()
+
+    def reload_from_json(self, json):
+        '''Replaces all attributes of obj dict'''
+        self.__dict__ = json
