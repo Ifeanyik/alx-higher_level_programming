1318799bfea7de39229105c5e9aea94e381fd115
Ifeanyik <jax64720@gmail.com>
2022-07-06 21:01:34 +0000

    Implement squareobject

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+Rectangle = __import__('9-rectangle').Rectangle
+
+class Square(Rectangle):
+    '''Defines square object attributes'''
+    def __init__(self, size):
+        '''Initialise object attributes'''
+        super().integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        '''Calculate Square object area'''
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Square] {}/{}".format(self.__size, self.__size)
