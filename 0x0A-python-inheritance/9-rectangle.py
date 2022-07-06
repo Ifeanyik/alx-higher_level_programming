60268879a38fa964a18be3a2f60d1c3de69bffce
Ifeanyik <jax64720@gmail.com>
2022-07-06 17:01:11 +0000

    Implement str attribute

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+'''Create class'''
+
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''defining class attributes'''
+    def __init__(self, width, height):
+        '''Initialiser'''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        '''computes area of object'''
+        return 2(self.__width + self.__height)
+
+    def __str__(self):
+        '''Called when str is called on an obj'''
+        f = "[Rectangle] {}/{}".format(self.__width, self.__height)
+        return f
