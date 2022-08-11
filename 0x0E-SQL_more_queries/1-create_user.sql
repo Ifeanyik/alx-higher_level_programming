aacced1d2dbd65008de86192fe36ae2f9c8634c9
Ifeanyik <jax64720@gmail.com>
2022-08-10 17:40:48 -0700

    Fixing edge cases

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Creates user on mysql server
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
+REVOKE ALL PRIVILEGES  ON *.* FROM 'user_0d_1'@'localhost';
 GRANT ALL PRIVILEGES ON *.* TO 'user_0d_1'@'localhost' WITH GRANT OPTION;
