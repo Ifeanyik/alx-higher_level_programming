417653a4cd4a603f44c5d8d1d55037d994de42e9
Ifeanyi <jax64720@gmail.com>
2022-09-05 18:51:45 +0000

    Calculate number factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const args = process.argv;
+function factorial (number) {
+  if (number != 0) {
+    return (number * factorial(number - 1));
+  } else {
+    return (1);
+  }
+}
+if (!args[2]) {
+  console.log('1');
+} else {
+  const arg = parseInt(args[2]);
+  if (!arg && arg != 0) {
+    console.log('NaN');
+  } else if (arg == 1) {
+    console.log(1);
+  } else if (arg == 0) {
+    console.log(1);
+  } else {
+    const fact = factorial(arg);
+    console.log(fact);
+  }
+}
