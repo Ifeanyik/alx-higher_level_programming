3e01d155ae3c03a5f08d6bd73aa8d86fbfe6b37d
Ifeanyi <jax64720@gmail.com>
2022-09-05 21:12:34 +0000

    Find second largest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,14 +6,16 @@ if (!args[2]) {
   console.log(0);
 } else {
   let prevVar = 0;
-  let largest = 0;
+  let largest = ;
   let i;
   for (i = 2; args[i]; i++) {
     if (args[i] > largest) {
       prevVar = largest;
       largest = args[i];
-    } if ((args[i] < largest) && args[i] > prevVar) {
+    } else if ((args[i] < largest) && args[i] > prevVar) {
       prevVar = args[i];
+    } else {
+      prevVar = args[1];
     }
   }
   console.log(prevVar);
