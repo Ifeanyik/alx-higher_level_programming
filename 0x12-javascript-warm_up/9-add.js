aa2364282c2997486248ae5616ed01158a9da06d
Ifeanyi <jax64720@gmail.com>
2022-09-05 18:30:53 +0000

    Adjustment to code style

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,14 +1,13 @@
 #!/usr/bin/node
 const args = process.argv;
-function add(a, b) {
+function add (a, b) {
   console.log(a + b);
 }
 if (!args[2] || !args[3]) {
   console.log('NaN');
-}
-else {
-  let var1 = parseInt(args[2]);
-  let var2 = parseInt(args[3]);
+} else {
+  const var1 = parseInt(args[2]);
+  const var2 = parseInt(args[3]);
   if (!var1 || !var2) {
     console.log('NaN');
   } else {
