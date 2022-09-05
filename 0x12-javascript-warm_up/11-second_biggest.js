6968760335f78d0a426072fe695c09b07b5a06a0
Ifeanyi <jax64720@gmail.com>
2022-09-05 21:43:28 +0000

    Manage edge case

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -5,18 +5,32 @@ if (!args[2]) {
 } else if (args[2] && !args[3]) {
   console.log(0);
 } else {
-  let prevVar = 0;
+  let secondLargest;
   let largest = args[2];
   let i;
-  for (i = 2; args[i]; i++) {
-    if (args[i] > largest) {
-      prevVar = largest;
-      largest = args[i];
-    } else if ((args[i] < largest) && args[i] > prevVar) {
-      prevVar = args[i];
+  for (i = 3; args[i]; i++) {
+    if (i === 3) {
+      if (args[i] > largest) {
+        secondLargest = largest;
+        largest = args[i];
+      } else {
+        if (args[i] != largest) {
+          secondLargest = args[i];
+        }
+      }
     } else {
-      prevVar = args[1];
+      if (args[i] > largest) {
+        secondLargest = largest;
+        largest = args[i];
+      } else {
+        if (!secondLargest) {
+          secondLargest = args[i];
+        }
+        if ((args[i] < largest) && (args[i] > secondLargest)) {
+          secondLargest = args[i];
+        }
+      }
     }
   }
-  console.log(prevVar);
+  console.log(secondLargest);
 }
