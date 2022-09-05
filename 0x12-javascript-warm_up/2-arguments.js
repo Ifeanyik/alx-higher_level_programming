665f072f75759fdf0a527c2cadacbac6f442c9a1
Ifeanyi <jax64720@gmail.com>
2022-09-05 13:14:31 +0000

    Semistandard conformities

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,11 +1,9 @@
 #!/usr/bin/node
 const args = process.argv;
 if (!args[2]) {
-	console.log('No argument');
+  console.log('No argument');
+} else if (args[2] && !args[3]) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
 }
-else if (args[2] && !args[3]) {
-	console.log('Argument found');
-}
-else {
-	console.log('Arguments found');
-}	
