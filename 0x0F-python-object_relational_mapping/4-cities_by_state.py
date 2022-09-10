8d188710074296923100a42580d09d4a6b8d2af4
Ifeanyik <jax64720@gmail.com>
2022-09-10 12:35:13 -0700

    Manage edge case

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -11,9 +11,12 @@ if __name__ == "__main__":
     my_host = 'localhost'
     c = dbase.connect(user=arg1, passwd=arg2, db=arg3, host=my_host, port=3306)
     q = c.cursor()
-    col1, col2, table = "cities.id", "cities.name", "cities"
-    q.execute("""SELECT * FROM {}
-            ORDER BY {} ASC""".format(table, col1))
-    rows = q.fetchall()
+    col1, table = "cities.id", "cities"
+    try:
+        q.execute("""SELECT * FROM {}
+                ORDER BY {} ASC""".format(table, col1))
+        rows = q.fetchall()
+    except dbase.Error:
+        exit()
     for row in rows:
         print(row)
