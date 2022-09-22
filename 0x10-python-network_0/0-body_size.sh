841ee7fdc08afd13419949a769c0d04d2cf38cdb
Ifeanyi <jax64720@gmail.com>
2022-09-22 13:45:32 -0700

    Print content size

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # this script prints the size in bytes of content downloaded with curl
-curl $1 -s -w "%{size_download}" | cut -b 11,12
+curl -sI "$1" | grep "Content-Length:" | cut -d " " -f 2
