9a74de2bd66159e7ab74ba01a3f8a9819bf3c532
Ifeanyik <jax64720@gmail.com>
2022-10-18 07:53:07 -0700

    Conform to code style standard

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const f_string = fs.writeFile(process.argv[2], process.argv[3], 'utf8', function(err) {
-  if (err)
-	console.error(err);
+fs.writeFile(process.argv[2], process.argv[3], 'utf8', function (err) {
+  if (err) { console.error(err); }
 });
