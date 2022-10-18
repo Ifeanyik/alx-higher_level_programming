e1f4328ee8f448944eb069b37900a780c4f69958
Ifeanyi <jax64720@gmail.com>
2022-10-18 10:41:16 -0700

    Request info from SWAPI

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 const request = require('request');
 const url = 'https://swapi-api.hbtn.io/api/films/' + process.argv[2];
-const options = {json: true};
+const options = { json: true };
 request(url, options, (error, response, body) => {
   if (error) {
     console.log(error);
-  };
-  if (!error && response.statusCode == 200) {
-    console.log(body['title']);
-  };
+  }
+  if (!error && response.statusCode === 200) {
+    console.log(body.title);
+  }
 });
